8f236826bfc5172497420df0e384658ee37b4923
broham1 <96927029+broham1@users.noreply.github.com>
2023-04-10 18:46:10 -0700

    Update poj3030.cpp

--- poj3030.cpp
+++ poj3030.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main(){
-	int iter, r, e, c, diff;
+	int iter, r, e, c;
 	cin >> iter;
 	for(int i = 0; i < iter; ++i){
 		cin >> r >> e >> c;
-		diff = e - c;
-		if(diff > r){
+		if((e-c) > r){
 			cout << "advertise\n";
 		}
-		else if (diff == r){
+		else if ((e-c) == r){
 			cout << "does not matter\n";
 		}
 		else{
